9164758d53cc93191aa2c66f928ca24d603c46e0
Dipika Kansara <68623123+DipikaKansara@users.noreply.github.com>
2021-09-27 10:47:35 +1000

    Initial Commit

---
+++ MVCArchitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVCArchitecture
+//
+//  Created by Dipika Kansara on 27/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
